596402565ef18ac1de7e8deeb1ddadd4b52439be
Edvonilson "Zell" Pereira <z-caps@hotmail.com>
2020-04-01 11:56:30 -0300

    atualização em 6.thisEGind2.js

--- 3.Funções/6.thisEBind2.js
+++ 3.Funções/6.thisEBind2.js
@@ -5,7 +5,21 @@ function Pessoa() {
     setInterval(function () {
         self.idade++
         console.log(self.idade)
-    }/*.bind(this*/, 1000)  //esse 1000 representa 1000 milisegundos representa quanto tempo leva entre cada execução.
+    }/*.bind(this)*/, 1000)  //esse 1000 representa 1000 milisegundos representa quanto tempo leva entre cada execução. - Se usar .bind(this) tbm daria certo no lugar do const self = this
+}
+
+new Pessoa
+
+
+// outra forma
+function Pessoa2() {
+    this.idade2 = 0
+
+
+    setInterval(function () {
+        this.idade2++
+        console.log(this.idade2)
+    }.bind(this), 3000)  //esse 3000 representa 1000 milisegundos representa quanto tempo leva entre cada execução. - Se usar .bind(this) tbm daria certo no lugar do const self = this
 }
 
 new Pessoa
