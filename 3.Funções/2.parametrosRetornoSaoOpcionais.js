7bb9a8073a80178ae417c29d67955815468a0fa4
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-03-31 21:29:44 -0300

    Update 2.parametrosRetornoSaoOpcionais.js

--- 3.Funções/2.parametrosRetornoSaoOpcionais.js
+++ 3.Funções/2.parametrosRetornoSaoOpcionais.js
@@ -1,14 +1,14 @@
-function area (largura, altura) {
-    const area = largura * altura 
-        if (area > 20) {
-        console.log(`Valor acima do permitido: ${area}m2.`)
-    } else {
-        return area
-    }
+function area(largura, altura) {
+  const areacalc = largura * altura
+  if (areacalc > 20) {
+    console.log(`Valor acima do permitido: ${areacalc}m2.`)
+  } else {
+    return areacalc
+  }
 }
 
 console.log(area(2, 2))
-console.log(area (2))
+console.log(area(2))
 console.log(area())
 console.log(area(2, 3, 17, 22, 44))
 console.log(area(5, 5))
