db1ff87e3b7742ed814d1c3a47d1b7ec208a2bab
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-03-31 22:34:34 -0300

    Update 4.paramPadrao.js

--- 3.Funções/4.paramPadrao.js
+++ 3.Funções/4.paramPadrao.js
@@ -15,7 +15,7 @@ console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
 
 function soma2(a, b, c) {
     a = a !== undefined ? a : 1
-    b = 1 in arguments ? b : 1
+    b = 1 in arguments ? b : 1  //1 por ser o índice 1 nos argumentos.
     c = isNaN(c) ? 1 : c
     return a + b + c
 
