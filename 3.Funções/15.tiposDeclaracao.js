ca981d3f286935560bf0ae94007f6824cd7d97b5
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-04-08 01:00:55 -0300

    ...

--- 3.Funções/15.tiposDeclaracao.js
+++ 3.Funções/15.tiposDeclaracao.js
@@ -1,16 +1,16 @@
-console.log(soma(3, 4)) // function declaration mesmo sendo feita depois é executada aqui
+console.log(soma(3, 4)) // function declaration mesmo sendo feita depois é executada aqui. O mesmo não acontece com a function expression
 
 // function declaration - mesmo antes de ser declarada ela já funciona
 function soma(x, y) {
-    return x + y
+  return x + y
 }
 
 //function expression
-const sub = function(x, y) {
-    return x - y
+const sub = function (x, y) {
+  return x - y
 }
-
+console.log(sub(10, 11))
 //named function expression (soma das 2 anteriores) - pouco usada
 const mult = function mult(x, y) {
-    return x * y
+  return x * y
 }
