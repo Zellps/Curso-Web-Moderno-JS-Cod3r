5abac85ca357b61a54663561273b226b698af841
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-04-14 23:11:58 -0300

    Update 19.factory1.js

--- 3.Funções/19.factory1.js
+++ 3.Funções/19.factory1.js
@@ -1,18 +1,17 @@
 // factory é uma função q retorna um objeto
 
 const prod1 = {
-    nome: "...",
-    preco: 45
+  nome: "...",
+  preco: 45,
 }
 
 // no lugar de criar vários prods...  prod1 prod2 prod3...
 
 function criarPessoa() {
-    return {
-        nome: "Ana"
-        sobrenome: "Silva"
-    }
+  return {
+    nome: "Ana",
+    sobrenome: "Silva",
+  }
 }
-
 
 console.log(criarPessoa())
