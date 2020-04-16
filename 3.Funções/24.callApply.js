3f0c5713df9adbd15fab9266dff8309bd9890891
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-04-16 15:00:52 -0300

    Update 24.callApply.js

--- 3.Funções/24.callApply.js
+++ 3.Funções/24.callApply.js
@@ -1,4 +1,7 @@
-function getPreco(imposto = 0, moeda = "RS") {
+// a diferença entre o call e o apply é a forma como vc passa os parâmetros na hora de chamar estes 2 métodos.
+
+
+function getPreco(imposto = 0, moeda = "R$") {
     return `${moeda} ${this.preco * (1 - this.desc) * (1 + imposto)}`
 }
 
@@ -20,6 +23,6 @@ const carro = {preco: 10000, desc: 0.2}
 console.log(getPreco.call(carro))
 console.log(getPreco.apply(carro))
 
-console.log(getPreco.call(carro, 0.17, "$"))
-console.log(getPreco.apply(carro, [0.17, "$"]))
+console.log(getPreco.call(carro, 0.17, "$")) // na call os parâmetros são passados diretamente.
+console.log(getPreco.apply(carro, [0.17, "$"])) // apply usa array para passar os parâmetros
 console.log(getPreco.apply(global, [0.18, "US$"]))
