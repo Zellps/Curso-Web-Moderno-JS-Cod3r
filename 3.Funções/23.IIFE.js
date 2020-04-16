a6814f3f8241ba5200308c0869a7befae141ed5c
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-04-15 23:43:18 -0300

    Update 23.IIFE.js

--- 3.Funções/23.IIFE.js
+++ 3.Funções/23.IIFE.js
@@ -4,3 +4,5 @@
     console.log('Será executado na hora')
     console.log('Foge do escopo mais abrangente')
 } )()
+
+//  função entre parênteses seguida de um par de parêntesis invoca a função na hora.
