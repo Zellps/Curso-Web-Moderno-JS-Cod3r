4391a40e3b407ad1e9b65a7139e85878ea8bfa4e
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-04-20 23:16:41 -0300

    ..

--- 3.1 Exercícios zell/exercicios07.js
+++ 3.1 Exercícios zell/exercicios07.js
@@ -1,16 +1,16 @@
-var number = 102300.99
-// prettier-ignore
-console.log(number.toLocaleString("pt-Br", { style: "currency", currency: "BRL" }))
+function bhaskara(ax2, bx, c) {
+  let resultados = []
+  //prettier-ignore
+  let delta = (bx ** 2) - (4 * ax2 * c)
+  if (delta <= 0) {
+    return "Delta negativo. lascou."
+  }
+  let x1 = (-bx + Math.sqrt(delta)) / (2 * ax2)
+  let x2 = (-bx - Math.sqrt(delta)) / (2 * ax2)
+  resultados.push(x1)
+  resultados.push(x2)
+  return resultados
+}
 
-number2 = 100000.88
-const format = { style: "currency", currency: "BRL" }
-console.log(number.toLocaleString(("pt-BR", format)))
-const hasICU = typeof Intl === "object"
-console.log(hasICU)
-
-const f1 = Intl.NumberFormat("br")
-f1.format(number2)
-console.log(
-  number2.toLocaleString("pt-Br", { style: "currency", currency: "BRL" })
-)
-
+console.log(bhaskara(1, 3, 2))
+console.log(bhaskara(3, 1, 2))
